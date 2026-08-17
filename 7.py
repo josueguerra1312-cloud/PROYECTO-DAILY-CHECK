c5ffad592e46dffd0e5358ed23fab64c950ef7c7
josueguerra1312-cloud <josueguerra1312@gmail.com>
2026-08-16 18:43:14 -0600

    Delete 7.py

--- 7.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-def hhmm_to_minutes(value):
-    if value is None:
-        return None
-    if isinstance(value, str):
-        value = value.strip()
-    dt = datetime.strptime(str(value), "%H:%M")
-    return dt.hour * 60 + dt.minute
