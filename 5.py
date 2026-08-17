62841a27a6c0bb56f40ce6c83ef9681dfe3760c9
josueguerra1312-cloud <josueguerra1312@gmail.com>
2026-08-16 18:43:00 -0600

    Delete 5.py

--- 5.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-def export_daily_check(df, output_file):
-    with pd.ExcelWriter(output_file, engine="xlsxwriter") as writer:
-        df.to_excel(writer, sheet_name="DAILY_CHECK", index=False)
